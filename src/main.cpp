f7a8ea4a4203834b1b45ba17353c0a6bb34d4689
Davide64-dev <davidpcu@hotmail.com>
2025-11-19 14:52:30 +0000

    feat: noar, led and temperature sensor working

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,66 @@
 #include <Arduino.h>
+#include <DHT.h>
+
+#define DHTPIN A7
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 int value = 0;
 float time_val  = 0;
 float distance = 0;
+float temperature = 0;
 
 void setup() {
   Serial.begin(9600);
+  
   pinMode(A1, OUTPUT);
   pinMode(A2, OUTPUT);
   pinMode(A3, OUTPUT);
-  pinMode(A6, INPUT);
 
   pinMode(D3, OUTPUT);
   pinMode(A0, INPUT);
-  digitalWrite(D3,LOW);
+  digitalWrite(D3, LOW);
+
+  dht.begin(); // Initialize DHT11
 }
 
-void loop() {
-  digitalWrite(D3,HIGH);
+void read_distance(){
+  digitalWrite(D3, HIGH);
   delayMicroseconds(10);
-  digitalWrite(D3,LOW);
+  digitalWrite(D3, LOW);
 
   time_val = pulseIn(A0, HIGH);
-  distance = (time_val * .0343)/2;
+  distance = (time_val * 0.0343) / 2;
+
   Serial.print("Distance: ");
-  if (distance < 10){
-    analogWrite(A1, 255);
-    analogWrite(A2, 0);
+  Serial.println(distance);
+}
+
+void read_temperature(){
+  temperature = dht.readTemperature();
+  if (isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
   }
   else{
+    Serial.print("Temperature: ");
+    Serial.println(temperature);
+  }
+}
+
+void logic_distance(){
+  if (distance < 10) {
+    analogWrite(A1, 255);
+    analogWrite(A2, 0);
+  } else {
     analogWrite(A1, 0);
     analogWrite(A2, 255);
   }
-  Serial.println(distance);
-  delay(100);
+}
+
+void loop() {
+  read_distance();
+  read_temperature();
+  logic_distance();
+  delay(1000);
 }
