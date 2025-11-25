00bc47f0448b837e87bd2635a2263481aad4b5bc
Davide64-dev <davidpcu@hotmail.com>
2025-11-25 12:09:15 +0000

    feat: added mqtt communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <Arduino.h>
 #include <DHT.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 #define DHTPIN A7
 #define DHTTYPE DHT11
+
+const char* ssid = "iPhone de Davide";
+const char* pass = "jatedisse";
+const char* brokerUser = "teensy";
+const char* brokerPass = "";
+const char* broker = "172.20.10.4";
+const char* distanceTopic = "/sonar/distance";
+const char* temperatureTopic = "/dht11/temperature";
+const char* humidityTopic = "/dht11/humidity";
+
+WiFiClient espClient;
+PubSubClient  client(espClient);
+long currentTime, lastTime; 
+char messages[50];
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -12,8 +29,39 @@ float distance = 0;
 float temperature = 0;
 float humidity = 0;
 
+void setupWiFi(){
+  delay(100);
+  Serial.print("\nConnecting to");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED){
+    delay(100);
+    Serial.print("-");
+  }
+
+  Serial.print("\nConnected to ");
+  Serial.println(ssid);
+}
+
+void reconnect(){
+  while (!client.connected()){
+    Serial.print("\nConnecting to ");
+    Serial.println(broker);
+    if (client.connect("172.20.10.4", brokerUser, brokerPass)){
+        Serial.print("\nConnected");
+    }else{
+      Serial.print("\nConnecting");
+      delay(5000);
+    }
+  }
+}
+
 void setup() {
   Serial.begin(9600);
+  setupWiFi();
+  client.setServer(broker, 1883);
   
   pinMode(A1, OUTPUT);
   pinMode(A2, OUTPUT);
@@ -75,5 +123,22 @@ void loop() {
   read_temperature();
   read_humidity();
   logic_distance();
+  if (!client.connected()){
+    reconnect();
+  }
+  client.loop();
+  currentTime = millis();
+  if (currentTime - lastTime > 2000){
+    snprintf(messages, 75, "%f", distance);
+    client.publish(distanceTopic, messages);
+
+    snprintf(messages, 75, "%f", temperature);
+    client.publish(temperatureTopic, messages);
+
+    snprintf(messages, 75, "%f", humidity);
+    client.publish(humidityTopic, messages);
+
+    lastTime = millis();
+  }
   delay(50);
 }
